9c9778b459afa4fa5b71fee0fb25dca3753a28d5
RivaldoPardede <rivaldopardede3@gmail.com>
2023-12-20 17:33:19 +0700

    transaction UI

---
+++ src/js/TWE.js
@@ -0,0 +1,2 @@
+import { Datepicker, Input, initTE } from "../../node_modules/tw-elements";
+initTE({ Datepicker, Input }, { allowReinits: true });
---
+++ src/js/transaction.js
@@ -0,0 +1,5 @@
+function showDropdownOptions() {
+    document.getElementById("options").classList.toggle("hidden");
+    document.getElementById("arrow-up").classList.toggle("hidden");
+    document.getElementById("arrow-down").classList.toggle("hidden");
+}
