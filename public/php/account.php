7461bbce060f95149d7fa5bc2af30eb1e1c329fb
RivaldoPardede <rivaldopardede3@gmail.com>
2023-12-21 17:42:37 +0700

    fixing bug

--- public/php/account.php
+++ public/php/account.php
@@ -279,11 +279,8 @@
                                     </dialog>
                                 </section>
     
-                                <a href="../index.php" class="btn rounded my-4 px-5 border-none py-2.5 overflow-hidden group bg-primary relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300 shadow-xl shadow-primary/50">Log Out
+                                <a href="../../src/php/signout.php" class="btn rounded my-4 px-5 border-none py-2.5 overflow-hidden group bg-primary relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300 shadow-xl shadow-primary/50">Log Out
                                     <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-                                    <?php
-                                        session_destroy();
-                                    ?>
                                 </a>
 
 
